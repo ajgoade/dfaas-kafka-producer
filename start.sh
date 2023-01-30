a23941eaefd7671e246e39340011a9fdbf2cdf40
Andy Goade <ajg135@gmail.com>
2023-01-30 16:38:28 -0600

    Updates

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-lima nerdctl run -p 9092:9092 \
+docker run -p 9092:9092 \
 -v "$(pwd)"/app:/app \
 -v $(pwd)/id_rsa:/sshtunnel_rsa:ro \
--e DFAAS_HOST=dfaasusr@<your dfaas ip> \
+-e DFAAS_HOST=<your dfaas user>@<your dfaas ip> \
 --rm vid_prod_container
